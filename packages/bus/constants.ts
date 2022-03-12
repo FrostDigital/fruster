ffbc1b80cd22e9008551def9de2b762cbabe37fb
Joel Soderstrom <joel.soderstrom@gmail.com>
2022-03-12 23:25:35 +0100

    style: code cleaning

--- packages/bus/constants.ts
+++ packages/bus/constants.ts
@@ -1,11 +1,5 @@
 export default {
-	ERROR_MESSAGE_NOT_CONNECTED: "Cannot use bus without connecting first. See bus.connect().",
-
 	METADATA_SUBJECT: "metadata",
-
-	NATS_PROTOCOL: "NATS",
-
-	HTTP_PROTOCOL: "HTTP",
 
 	CONTENT_ENCODING_GZIP: "gzip",
 
