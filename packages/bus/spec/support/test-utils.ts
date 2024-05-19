f12beb8f5b95cbdd37ff16bc42bac386ca5cf0c6
Joel Soderstrom <joel.soderstrom@gmail.com>
2024-05-19 18:11:32 +0200

    chore: spawns nats-server rather then using old nsc package

--- packages/bus/spec/support/test-utils.ts
+++ packages/bus/spec/support/test-utils.ts
@@ -1,25 +1,50 @@
-const nsc = require("nats-server-control");
+import { spawn } from "child_process";
 import { Client } from "nats";
 import bus from "../../index";
 
-export const startNatsServer = (port?: number) => {
-	const natsServerPort = port || Math.floor(Math.random() * 60000 + 2000);
-	const natsUrl = "nats://localhost:" + natsServerPort;
+export function startNatsServer(port: number) {
+	return new Promise<TestConnection>((resolve, reject) => {
+		const natsServerPort = port || Math.floor(Math.random() * 60000 + 2000);
+		const natsUrl = "nats://localhost:" + natsServerPort;
 
-	let connection = {
-		server: null,
-		natsUrl: natsUrl,
-		port: natsServerPort,
-	};
+		const natsProcess = spawn("nats-server", ["-p", natsServerPort.toString()], {
+			stdio: "ignore", // change to "inherit" to debug
+		});
 
-	return nsc.startServer(natsServerPort).then((natsServer: any) => {
-		connection.server = natsServer;
-		return connection;
+		natsProcess.on("error", (error) => {
+			console.log(error);
+			reject(error);
+		});
+
+		natsProcess.on("close", (code) => {
+			if (code !== 0) {
+				reject(new Error(`NATS server exited with code ${code}`));
+			}
+		});
+
+		setTimeout(() => {
+			resolve({
+				server: {
+					kill: () => {
+						return new Promise<void>((resolveKill) => {
+							natsProcess.on("close", () => {
+								resolveKill();
+							});
+
+							natsProcess.kill();
+						});
+					},
+				},
+				natsUrl,
+				port: natsServerPort,
+			});
+		}, 250);
 	});
-};
-
+}
 export interface TestConnection {
-	server?: any;
+	server: {
+		kill: () => Promise<void>;
+	};
 	client?: Client;
 	natsUrl: string;
 	port: number;
@@ -30,16 +55,16 @@ export const startNatsServerAndConnectBus = (port?: number, schemasDir?: string)
 	const natsUrl = "nats://localhost:" + natsServerPort;
 
 	let connection: TestConnection = {
+		// @ts-ignore
 		server: undefined,
 		client: undefined,
 		natsUrl: natsUrl,
 		port: natsServerPort,
 	};
 
-	return nsc
-		.startServer(natsServerPort)
-		.then((natsServer: any) => {
-			connection.server = natsServer;
+	return startNatsServer(natsServerPort)
+		.then((natsServer) => {
+			connection.server = natsServer.server;
 			return connection;
 		})
 		.then(() =>
